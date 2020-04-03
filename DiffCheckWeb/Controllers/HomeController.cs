c797390e5131fc3d6f3df052a362488612f9c6a8
Jaskaran Singh <jaskarannagi19@gmail.com>
2020-04-03 07:40:24 +0530

    "Final commit"

---
+++ DiffCheckWeb/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DiffCheckWeb.Models;
+using DiffMatchPatch;
+using Newtonsoft.Json;
+
+namespace DiffCheckWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public JsonResult Diff([FromBody]JobModel jobList)//MAPPING CH PROBLEM
+        {
+
+            List<Diff> diff = new List<Diff>();
+            List<Diff> result = new List<Diff>();
+            diff_match_patch dmp = new diff_match_patch();
+
+            string title = jobList.JobClass[0].title;
+            string newTitle = jobList.JobClass[1].title;
+
+            string oldDescription = jobList.JobClass[0].description;
+            string newDescription = jobList.JobClass[1].description;
+
+
+            diff = dmp.diff_main(title, newTitle);
+            dmp.diff_cleanupSemantic(diff);
+
+
+
+            for (int j = 0; j < diff.Count; j++)
+            {
+                result.Add(diff[j]);
+            }
+
+            //Compare description
+            diff = dmp.diff_main(oldDescription, newDescription);
+            dmp.diff_cleanupSemantic(diff);
+            for (int j = 0; j < diff.Count; j++)
+            {
+                result.Add(diff[j]);
+            }
+            string output = JsonConvert.SerializeObject(result);
+
+            return Json(output);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DiffCheckWeb/Models/JobModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiffCheckWeb.Models
+{
+    public class JobModel
+    {
+        public Job[] JobClass { get; set; }
+    }
+    public class Job
+    {
+        public string title { get; set; }
+        //public string date { get; set; }
+        //public string referencenumber { get; set; }
+        //public string url { get; set; }
+
+
+
+        //public string company { get; set; }
+        //public string city { get; set; }
+        //public string state { get; set; }
+        //public string country { get; set; }
+        //public string postalcode { get; set; }
+        //public string salary { get; set; }
+        //public string education { get; set; }
+        //public string jobtype { get; set; }
+        //public string officeid { get; set; }
+        //public string categories { get; set; }
+        //public string vakgebied { get; set; }
+        //public string functions { get; set; }
+        //public string experience { get; set; }
+        //public string vacancy_reason { get; set; }
+        public string description { get; set; }
+        //public string sponsored { get; set; }
+        //public string budget { get; set; }
+        //public string email { get; set; }
+    }
+
+
+}
